c1476bfd8f4725a00fbba0a97b54c0f8d652187e
Daniel Costa <daniel.hfc96@gmail.com>
2026-03-18 16:12:06 +0000

    monkey patch

--- setup.py
+++ setup.py
@@ -1,14 +1,31 @@
 import sys
 import os
-import sysconfig
+from setuptools import setup
+from setuptools.command.build_ext import build_ext
 
-# On non-Windows, prevent setuptools from generating .def export files
-# which Linux gcc cannot handle
-if sys.platform != "win32":
-    # Remove EXT_SUFFIX patterns that trigger .def generation
-    pass
-else:
-    # Force MinGW compiler on Windows
+
+class BuildExtNoDef(build_ext):
+    """Custom build_ext that removes .def files from the link step on Linux/Mac.
+
+    cffi generates .abi3.def export files which are only valid on Windows (MinGW).
+    On Linux, gcc treats them as linker scripts and fails.
+    """
+    def build_extension(self, ext):
+        # Save original link method
+        original_link = self.compiler.link_shared_object
+
+        def patched_link(objects, *args, **kwargs):
+            # Filter out .def files on non-Windows
+            if sys.platform != "win32":
+                objects = [o for o in objects if not o.endswith(".def")]
+            return original_link(objects, *args, **kwargs)
+
+        self.compiler.link_shared_object = patched_link
+        super().build_extension(ext)
+        self.compiler.link_shared_object = original_link
+
+
+if sys.platform == "win32":
     os.environ.setdefault("CC", "gcc")
     if "build_ext" not in sys.argv:
         sys.argv.insert(1, "build_ext")
@@ -19,8 +36,7 @@ else:
         if isinstance(cfg[key], str) and '/MD' in cfg[key]:
             cfg[key] = cfg[key].replace('/MD', '')
 
-from setuptools import setup
-
 setup(
     cffi_modules=["spectrofast/_fft_build.py:ffibuilder"],
+    cmdclass={"build_ext": BuildExtNoDef},
 )
